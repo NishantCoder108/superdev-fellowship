e9685e8b453ce58fb15f07229d9fdebf155c4343
Nishant <69218488+NishantCoder108@users.noreply.github.com>
2025-07-18 10:56:47 +0530

    Write custom derive macro

--- cus-derive-macro/serialize_macro/src/lib.rs
+++ cus-derive-macro/serialize_macro/src/lib.rs
@@ -1,14 +1,108 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{Data, DeriveInput, Fields};
+
+#[proc_macro_derive(SerializeNumberStruct)]
+pub fn serialise_number_struct(input: TokenStream) -> TokenStream {
+    let ast: DeriveInput = syn::parse(input).unwrap();
+    let name = &ast.ident;
+
+    let serialize_fields = match &ast.data {
+        Data::Struct(data_struct) => {
+            match &data_struct.fields {
+                Fields::Named(fields) => {
+                    let field_serializations = fields.named.iter().map(|field| {
+                        let field_name = &field.ident;
+                        quote! {
+                            result.extend_from_slice(&self.#field_name.to_be_bytes());
+                        }
+                    });
+                    /*
+                       field_serializeations = [quote!(result.extend_from_slice(&self.qty_1.to_be_bytes())), quote!(result.extend_from_slice(&self.qty_2.to_be_bytes()))]
+                    */
+                    quote! {
+                        #(#field_serializations)*
+                    }
+                }
+                _ => panic!("Only named fields are supported"),
+            }
+        }
+        _ => panic!("Only structs are supported"),
+    };
+    /*
+       serialize_fields ->
+       result.extend_from_slice(&self.qty_1.to_be_bytes())
+       result.extend_from_slice(&self.qty_2.to_be_bytes())
+       result.extend_from_slice(&self.qty_3.to_be_bytes())
+    */
+
+    let generated = quote! {
+        impl Serialize for #name {
+            fn serialize(&self) -> Vec<u8> {
+                let mut result = Vec::new();
+                #serialize_fields
+                result
+            }
+        }
+    };
+    generated.into()
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[proc_macro_derive(DeserializeNumberStruct)]
+pub fn deserialise_number_struct(input: TokenStream) -> TokenStream {
+    let ast: DeriveInput = syn::parse(input).unwrap();
+    let name = &ast.ident;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    let (deserialize_fields, field_assignments, total_size) = match &ast.data {
+        Data::Struct(data_struct) => match &data_struct.fields {
+            Fields::Named(fields) => {
+                let mut offset: usize = 0;
+                let mut field_deserializations = Vec::new();
+                let mut field_assignments = Vec::new();
+
+                for field in &fields.named {
+                    let field_name = &field.ident;
+                    let field_size = 4;
+                    let start_offset = offset;
+                    let end_offset = offset + field_size;
+
+                    field_deserializations.push(quote! {
+                        let #field_name = {
+                            let bytes: [u8; 4] = base[#start_offset..#end_offset]
+                                .try_into()
+                                .map_err(|_| Error)?;
+                            i32::from_be_bytes(bytes)
+                        };
+                    });
+
+                    field_assignments.push(quote! {
+                        #field_name
+                    });
+
+                    offset += field_size;
+                }
+
+                (field_deserializations, field_assignments, offset)
+            }
+            _ => panic!("Only named fields are supported"),
+        },
+        _ => panic!("Only structs are supported"),
+    };
+
+    let generated = quote! {
+        impl Deserialize for #name {
+            fn deserialize(base: &[u8]) -> Result<Self, Error> {
+                if base.len() < #total_size {
+                    return Err(Error);
+                }
+
+                #(#deserialize_fields)*
+
+                Ok(#name {
+                    #(#field_assignments,)*
+                })
+            }
+        }
+    };
+    generated.into()
 }
