af3a9eef3c38b16ebd13bd147b3ef825aca4f8b5
Nishant <69218488+NishantCoder108@users.noreply.github.com>
2025-07-29 10:32:19 +0530

    Add another approach to solve week1 assignment

--- week1/assignment1/traity_trait/src/lib.rs
+++ week1/assignment1/traity_trait/src/lib.rs
@@ -62,3 +62,77 @@ fn pascal_case(chars: &String) -> String {
         })
         .collect()
 }
+
+/**
+ * *_____________Prev Another Approach:________________________
+ * 
+ * #![crate_type = "proc-macro"]
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{Data, DeriveInput, Fields, FieldsNamed, parse_macro_input};
+
+fn todo_app_impl(input: DeriveInput) -> Result<proc_macro2::TokenStream, syn::Error> {
+    let struct_name = &input.ident;
+    let struct_vis = &input.vis;
+    let fields = match &input.data {
+        Data::Struct(data) => match &data.fields {
+            Fields::Named(fields) => fields,
+            _ => {
+                return Err(syn::Error::new_spanned(
+                    struct_name,
+                    "Expected a struct with named fields",
+                ));
+            }
+        },
+        _ => return Err(syn::Error::new_spanned(struct_name, "Expected a struct")),
+    };
+
+    let field_names = fields
+        .named
+        .iter()
+        .map(|field| {
+            let field_name = &field.ident.as_ref().unwrap();
+            let field_ty = &field.ty;
+            let pascal = to_pascal_case(&field_name.to_string());
+            let renamed = format!("TodoApp{}", pascal);
+
+            quote! {
+                #[serde(rename=#renamed)]
+                #field_name: #field_ty
+            }
+        })
+        .collect::<Vec<_>>();
+
+    let token = quote! {
+        #[derive(serde::Serialize, serde::Deserialize)]
+        #struct_vis struct #struct_name {
+            #(#field_names),*
+        }
+
+
+    };
+
+    Ok(token)
+}
+
+#[proc_macro_attribute]
+pub fn todo_app(_args: TokenStream, _input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(_input as DeriveInput);
+    match todo_app_impl(input) {
+        Ok(tokens) => tokens.into(),
+        Err(err) => err.to_compile_error().into(),
+    }
+}
+
+fn to_pascal_case(s: &str) -> String {
+    s.split('_')
+        .map(|word| {
+            let mut chars = word.chars();
+            match chars.next() {
+                Some(f) => f.to_uppercase().collect::<String>() + chars.as_str(),
+                None => "".to_string(),
+            }
+        })
+        .collect()
+}
+ */
