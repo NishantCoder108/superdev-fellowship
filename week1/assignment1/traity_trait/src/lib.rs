09c7e6ec3a926c09439ded83ff873be0d5ecf8ab
Nishant <69218488+NishantCoder108@users.noreply.github.com>
2025-07-29 10:06:23 +0530

    Submission assignment1

--- week1/assignment1/traity_trait/src/lib.rs
+++ week1/assignment1/traity_trait/src/lib.rs
@@ -1,9 +1,44 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, Data, DeriveInput, Fields, FieldsNamed};
+use syn::{parse_macro_input, Data, DeriveInput, Error, Fields};
 
-fn todo_app_impl(input: DeriveInput) -> Result<proc_macro2::TokenStream, syn::Error> {    
-   
+fn todo_app_impl(input: DeriveInput) -> Result<proc_macro2::TokenStream, syn::Error> {
+    let input_data = input.clone();
+    let name = input_data.ident;
+    let attrs = input_data.attrs;
+    match input_data.data {
+        Data::Struct(todo_struct) => match todo_struct.fields {
+            Fields::Named(todo_fields) => {
+                let todo_field = todo_fields
+                    .named
+                    .iter()
+                    .map(|todo_field_item| {
+                        let field_name = todo_field_item.ident.as_ref().unwrap();
+                        let field_ty = &todo_field_item.ty;
+                        let pascal_converter = pascal_case(&field_name.to_string());
+                        let renamed = format!("TodoApp{}", pascal_converter);
+
+                        quote! {
+                            #[serde(rename = #renamed)]
+                            #field_name : #field_ty
+                        }
+                    })
+                    .collect::<Vec<_>>();
+
+                Ok(quote! {
+                    #(#attrs)*
+                    struct #name {
+                        #(#todo_field),*
+                    }
+                })
+            }
+            _ => Err(Error::new_spanned(
+                input,
+                "Only struct with name fields are supported",
+            )),
+        },
+        _ => Err(Error::new_spanned(input, "Only structs are supported")),
+    }
 }
 
 #[proc_macro_attribute]
@@ -13,4 +48,17 @@ pub fn todo_app(_args: TokenStream, input: TokenStream) -> TokenStream {
         Ok(tokens) => tokens.into(),
         Err(err) => err.to_compile_error().into(),
     }
+}
+
+fn pascal_case(chars: &String) -> String {
+    chars
+        .split("_")
+        .map(|char_item| {
+            let mut word = char_item.chars();
+            match word.next() {
+                Some(c) => c.to_uppercase().chain(word).collect::<String>(),
+                None => String::new(),
+            }
+        })
+        .collect()
 }
